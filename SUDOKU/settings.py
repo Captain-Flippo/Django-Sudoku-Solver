e573704ab8e74561b13d5b39cde1e23668a779e3
Captain-Flippo <100761656+Captain-Flippo@users.noreply.github.com>
2022-03-07 16:35:51 +0100

    Update settings.py
    changed secret key

--- SUDOKU/settings.py
+++ SUDOKU/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6^8i_xv%nb0qc^kgsrik24pt(#5!z3t*qb9)j1=2rbmz!u@=6h'
+SECRET_KEY = 'some insecure secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
